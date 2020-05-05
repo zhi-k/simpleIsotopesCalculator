812158fa3ce9e72c4243bf3b9225a803e2b0deca
Zhi Kang <chongz.kang@gmail.com>
2020-05-05 15:20:29 +0800

    Added selector to select molecule and send back to context store

--- src/components/ChooseMolecule.js
+++ src/components/ChooseMolecule.js
@@ -1,7 +1,41 @@
 import React from "react";
-import { f18 } from "../helpers/Molecules";
+import { useStateValues } from "../context/Store";
 
 export default function ChooseMolecule() {
+  const [state, dispatch] = useStateValues();
+
+  function handleSelect(e) {
+    e.preventDefault();
+
+    let halfLife = state.settings.halfLife;
+    let molecule = state.settings.molecule;
+
+    switch (e.target.value) {
+      case "f18":
+        halfLife = 109.77;
+        molecule = "Fluorine-18";
+        break;
+      case "ga68":
+        halfLife = 68;
+        molecule = "Gallium-68";
+        break;
+      case "lu177":
+        halfLife = 9571.68; // equivalent to 6.647 days
+        molecule = "Lutetium-177";
+        break;
+      default:
+        throw new Error(`Unable to set half life`);
+    }
+
+    dispatch({
+      type: "SET_MOLECULE",
+      payload: {
+        molecule,
+        halfLife,
+      },
+    });
+  }
+
   return (
     <div className="input-group input-group-sm mb-3 w-50" id="molecule">
       <div className="input-group-prepend">
@@ -9,7 +43,7 @@ export default function ChooseMolecule() {
           Molecule
         </label>
       </div>
-      <select className="custom-select" id="moleculeSelect">
+      <select className="custom-select" id="moleculeSelect" onChange={handleSelect}>
         <option selected value="f18">
           Fluorine-18
         </option>
