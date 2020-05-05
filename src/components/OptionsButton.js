cc361dfeb13c37bbbbe89f1929c113da8743a8b1
Zhi Kang <chongz.kang@gmail.com>
2020-05-05 20:12:44 +0800

    Allow delete of isotopes in modal

--- src/components/OptionsButton.js
+++ src/components/OptionsButton.js
@@ -43,45 +43,61 @@ function CenteredModal(props) {
   }
 
   return (
-    <Modal {...props} size="sm" centered>
+    <Modal {...props} size="md" centered>
       <Modal.Header closeButton>
         <Modal.Title className="d-flex text-align-center">
-          <div className="h4">Add custom isotope</div>
+          <div className="h4">Custom Isotope</div>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body id="modalBody">
         <form className="form-group w-100 pt-1 mb-1 w-auto" onSubmit={handleSubmit}>
-          <div className="mb-2 mr-sm-2 col-md-5">
-            <label htmlFor="isotope" className="ml-1">
-              Isotope
+          <div className="mb-2 mr-sm-2 col-md-12">
+            <label htmlFor="isotope" className="ml-1 mr-1">
+              Isotope:
             </label>
             <input type="text" name="isotope" className="ml-1" onChange={handleChange} />
           </div>
-          <div className="mb-2 mr-sm-2 col-md-5 w-auto">
-            <label htmlFor="halfLife" className="ml-1">
-              Half Life (minutes)
+          <div className="mb-2 mr-sm-2 col-md-12 w-auto">
+            <label htmlFor="halfLife" className="ml-1 mr-1">
+              Half Life (minutes):
             </label>
             <input type="text" name="halfLife" className="ml-1" onChange={handleChange} />
           </div>
-          <button hidden type="submit"></button>
+          <button type="submit" onClick={handleSubmit} className="btn btn-primary btn mx-3">
+            Save
+          </button>
         </form>
       </Modal.Body>
       <Modal.Footer id="modalFooter">
-        <button type="submit" onClick={handleSubmit} className="btn btn-primary btn">
-          Save
-        </button>
+        <ul className="list-group w-100 flex-wrap">
+          {state.options.map((option, index) => (
+            <li
+              key={index}
+              className="list-group-item w-100 d-flex justify-content-between align-items-center"
+              id="customList"
+            >
+              {option.optionName}
+              <button
+                className="btn btn-danger btn-small m-2"
+                onClick={() => dispatch({ type: "DELETE_OPTION", payload: option })}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
       </Modal.Footer>
     </Modal>
   );
 }
 
 export default function OptionsButton() {
-  const [modalShow, setModalShow] = useState(false);
+  const [modalShow, setModalShow] = useState(true);
 
   return (
     <>
-      <button className="btn btn-success mx-4 btn-sm" onClick={() => setModalShow(true)}>
-        Add Custom Isotopes
+      <button className="btn btn-success mx-4 btn-sm" onClick={() => setModalShow(true)} id="modalButton">
+        Add or Remove Custom Isotopes
       </button>
       <CenteredModal show={modalShow} onHide={() => setModalShow(false)} />
     </>
