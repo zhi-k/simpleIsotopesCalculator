193da837811007dc9a7cc982a25631e9f9e91dbc
zhi-k <chongz.kang@gmail.com>
2020-11-01 10:57:36 +0800

    Testing GTM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import TagManager from "react-gtm-module";
+
 import Heading from "./components/Heading";
 import Calculator from "./components/Calculator";
 import { StoreProvider, initialState, rootReducer } from "./context/Store";
 
+TagManager.initialize(tag);
+
 function App() {
+  window.dataLayer.push({
+    event: "pageview",
+    page: {
+      url: location,
+      title: title,
+    },
+  });
+
   return (
     <StoreProvider state={initialState} reducer={rootReducer}>
       <div className="container-fluid" id="background">
